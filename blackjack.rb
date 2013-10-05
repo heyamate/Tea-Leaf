914b69866725b4364edeb3fc1ef80b6545b8d505
Heyamate <heyamate@gmail.com>
2013-10-05 13:37:16 +0800

    Blackjack completed
    blackjack now working.
    Saves name as well

--- blackjack.rb
+++ blackjack.rb
@@ -15,7 +15,7 @@ def calculate_total(cards)
 		end	
 	end
 
-	#correction for Aces
+	# => Correction for Aces
 	hand.select{|e| e == "Ace"} .count.times do
 		total -= 10 if total > 21
 	end
@@ -24,18 +24,28 @@ def calculate_total(cards)
 end
 
 def show_hand(cards)
-	hand 
+	cards.each do |v|
+		test = v.reverse
+		puts "A #{test[0]} of #{test[1]}"
+	end
 end
-puts "Welcome to Blackjack"
 
-#create deck
-suit = ['Heart', 'Diamond', 'Spade', 'Club']
+# => Start Game
+
+puts "Welcome to Blackjack"
+puts "What is your name?"
+name = gets.chomp
+puts "Ok, #{name} lets deal!"
+name.capitalize!
+# => Create Deck
+
+suit = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
 cards = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King', 'Ace']
 
 deck = suit.product(cards)
 deck.shuffle!
 
-#Deal
+# => Deal Cards
 
 my_hand = []
 dealer_hand = []
@@ -46,13 +56,113 @@ dealer_hand = []
 end
 
 my_amount = calculate_total(my_hand)
+dealer_amount = calculate_total(dealer_hand)
 
-puts "You have in your hand #{my_hand}"
+# => Show Cards
+
+puts "In your hand you have:"
+show_hand(my_hand)
 puts "Your total is #{my_amount}"
+puts ""
+puts "The dealer has:"
+show_hand(dealer_hand)
+puts "Their total is #{dealer_amount}"
 
-binding.pry
+if my_amount == 21
+	puts "You got Blackjack! Congratuations #{name}, you win!!!"
+	exit
+elsif dealer_amount == 21
+	puts "Dealer got Blackjack. Sorry #{name}. You lose"
+	exit
+end
 
-puts "Did you all want to:"
-puts "1) Hit or"
-puts "2) Stay"
-gets.chomp
+# => Players Turn
+
+while my_amount < 21
+	puts ""
+	puts "Did you all want to:"
+	puts "1) Hit or"
+	puts "2) Stay"
+	hit_or_stay = gets.chomp
+	
+	# => Error checking
+
+	if !["1","2"].include?(hit_or_stay)
+		puts "You can only choose 1 or 2"
+		next
+	end
+
+	# => Hit
+
+	if hit_or_stay == "1"
+		my_hand << deck.pop
+		my_amount = calculate_total(my_hand)
+		puts ""
+		puts "You now have:"
+		show_hand(my_hand)
+		puts "Your total is #{my_amount}"
+		if my_amount == 21
+			puts "You got Blackjack! Contratulations #{name}, you win!"
+			exit
+		elsif my_amount > 21
+			puts "Looks like you busted #{name}. You are out"
+			exit
+		end
+	end
+
+	# => Stay
+
+	if hit_or_stay == "2"
+		puts "You have chosen to stay"
+		break
+	end
+end
+
+# => Dealers Turn
+
+while dealer_amount < 18
+	puts "Dealer has chosen to hit..."
+	dealer_hand << deck.pop
+	dealer_amount = calculate_total(dealer_hand)
+	puts ""
+	puts "The dealer has:"
+	show_hand(dealer_hand)
+	puts "Their total is #{dealer_amount}"
+end
+
+if dealer_amount == 21
+	puts "Dealer has Blackjack. #{name} loses"
+	exit
+elsif dealer_amount > 21
+	puts "Dealer busts. #{name} wins!"
+	exit
+end
+
+# => End game
+
+if dealer_amount > my_amount
+	puts "Dealer has the higher hand. #{name} loses."
+	exit
+elsif dealer_amount < my_amount
+	puts "You have the higher hand. #{name} wins!"
+	exit
+else
+	puts "The game is a draw"
+	exit
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
